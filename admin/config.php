21f76ee4767b235525e8c28a0b0a942a25f4d265
anuj331991 <anujrajput25@yahoo.com>
2015-08-17 21:56:02 +0530

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://firstplaceshop.localhost.com/admin/');
+define('HTTP_CATALOG', 'http://firstplaceshop.localhost.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://firstplaceshop.localhost.com/admin/');
+define('HTTPS_CATALOG', 'http://firstplaceshop.localhost.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/firstplaceshop/admin/');
+define('DIR_SYSTEM', '/var/www/html/firstplaceshop/system/');
+define('DIR_LANGUAGE', '/var/www/html/firstplaceshop/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/firstplaceshop/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/firstplaceshop/system/config/');
+define('DIR_IMAGE', '/var/www/html/firstplaceshop/image/');
+define('DIR_CACHE', '/var/www/html/firstplaceshop/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/firstplaceshop/system/download/');
+define('DIR_UPLOAD', '/var/www/html/firstplaceshop/system/upload/');
+define('DIR_LOGS', '/var/www/html/firstplaceshop/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/firstplaceshop/system/modification/');
+define('DIR_CATALOG', '/var/www/html/firstplaceshop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'tech');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://firstplaceshop.localhost.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://firstplaceshop.localhost.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/firstplaceshop/catalog/');
+define('DIR_SYSTEM', '/var/www/html/firstplaceshop/system/');
+define('DIR_LANGUAGE', '/var/www/html/firstplaceshop/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/firstplaceshop/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/firstplaceshop/system/config/');
+define('DIR_IMAGE', '/var/www/html/firstplaceshop/image/');
+define('DIR_CACHE', '/var/www/html/firstplaceshop/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/firstplaceshop/system/download/');
+define('DIR_UPLOAD', '/var/www/html/firstplaceshop/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/firstplaceshop/system/modification/');
+define('DIR_LOGS', '/var/www/html/firstplaceshop/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'tech');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
